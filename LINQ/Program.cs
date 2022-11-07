d9f90090da33b2c707859429c29a74d2c294f6d7
Mike Cowell <spike@spikeandbetty.com>
2022-11-07 14:35:32 -0500

    init commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace LINQ
+﻿using System.Transactions;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var libraryOfLinks = new List<string>() { 
+                "Breath of the Wild", 
+                "The Legend of Zelda", 
+                "Link's Awakening", 
+                "Super Smash Brothers", 
+                "A Link To The Past",
+                "Link's Awakening",
+                "Ocarina of Time",
+                "Majora's Mask",
+                "The Wind Waker",
+                "Twilight Princess",
+                "Skyward Sword",
+                "Mario Kart",
+                "A Link Between Worlds",
+                "Tears of a Kingdom"
+            };
+            libraryOfLinks = libraryOfLinks.OrderBy(x => x.Count()).ToList();
+            foreach (var link in libraryOfLinks)
+            {
+                Console.WriteLine(link);              
+            }
         }
     }
 }
